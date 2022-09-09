1907c9f9b68141728ece86591ce5a27669054907
Jerethink <jerethink@gmail.com>
2022-09-08 21:47:02 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 
 /**
- *main - This is the entry point of the project
+ * main - This is the entry point of the project
  *
  * Return: always 0
- * /
- 
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle"};
- return (0);
- }
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
